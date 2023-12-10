3d6bbbac0527543a61da2e299178d3130db23357
Usama Tahir <usamatahir7866@gmail.com>
2023-12-10 22:08:36 +0100

    singup page added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,11 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { CommonModule } from '@angular/common';
-
 import { MatInputModule } from '@angular/material/input';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { FormsModule } from '@angular/forms';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 @NgModule({
   declarations: [LoginComponent, SignupComponent],
   imports: [
@@ -15,6 +15,7 @@ import { FormsModule } from '@angular/forms';
     MatInputModule,
     FormsModule,
     CommonModule,
+    MatCheckboxModule,
   ],
   bootstrap: [],
 })
--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,8 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.scss',
 })
-export class SignupComponent {}
+export class SignupComponent {
+  loading = false;
+
+  signMeUp() {
+    this.loading = true;
+
+    setTimeout(() => {
+      this.loading = false;
+    }, 5000);
+  }
+}
